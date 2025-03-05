5b3c7c8ab7bd7b9c35e40f613717cde3356ed32c
marilyn.musyimi <marilyn.musyimi@student.moringaschool.com>
2025-03-05 20:35:54 +0300

    spin-nd-win-game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import SpinWheel from './components/SpinWheel';
+import './index.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1> } />
-
+    <Routes>  {/* Ensure Routes is used inside BrowserRouter */}
+      <Route path="/" element={<SpinWheel />} />
+      {}
     </Routes>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';  // Ensure BrowserRouter is imported
+import App from './App';  // Import your App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+root.render(
+  <BrowserRouter>  {/* Wrap your app with BrowserRouter */}
+    <App />
+  </BrowserRouter>
+);
