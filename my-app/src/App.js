a97b5b9136b8430510a709191af73a0e395a8009
marilyn.musyimi <marilyn.musyimi@student.moringaschool.com>
2025-03-05 18:04:48 +0300

    starting point

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1> } />
+
+    </Routes>
+  );
+}
+
+export default App;
+
